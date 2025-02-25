ba46b66ad0ae1bd2a200b5644cf945bd9cfef95b
leonehynce <leonehynce034@gmail.com>
2025-02-25 12:49:18 +0300

    initial commit

--- myapp/models.py
+++ myapp/models.py
@@ -6,7 +6,9 @@ class BlogPost(models.Model):
     content = models.TextField()
     author = models.CharField(max_length=30)
     created_at =models.DateTimeField(auto_now=True)
-    email =models.CharField(max_length=200)
+    # email =models.CharField(max_length=200)
+    email = models.EmailField (null=True ,blank=True)
+    phone = models.CharField (null=True ,max_length=20)
 
 
     def __str__(self):
