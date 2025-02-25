77428a0dfdf08f30227e6aa34778683375fb4a08
leonehynce <leonehynce034@gmail.com>
2025-02-25 11:32:10 +0300

    initial commit

---
+++ myapp/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import BlogPost
+
+# Create your views here.
+def index(request):
+       posts=BlogPost.objects.all
+       return render(request,'index.html',{'posts':posts})
